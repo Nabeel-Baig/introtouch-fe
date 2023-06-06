c595da511ea9c67c387fcb10537101fb1c15c81e
Samair <samairzia1995@gmail.com>
2023-06-06 23:00:58 +0500

    TS-25 - Publick Preview button on bottom right

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import chart from '../../assets/icons/nav/chart.png';
 import qr from '../../assets/icons/nav/qr.png';
 import Navlink from './Navlink';
 import './styles.css'
+import { Link } from 'react-router-dom';
 
 const navlinks = [
   { title: 'Home', icon: home, to: '/' },
@@ -14,15 +15,19 @@ const navlinks = [
 
 const Navbar = () => {
   return (
+    <>
+    <div className="join-btn-wrapper">
+      <Link to="" className="join-btn">Join Intro Touch</Link>
+    </div>
     <div
       id="navabar"
       className=" bg-black text-white flex justify-around p-3 fixed-navbar z-50"
-      // className="fixed sm:sticky bottom-0  left-0 right-0 bg-brand-brown flex justify-around p-3"
     >
       {navlinks.map((link, i) => (
         <Navlink data={link} key={i} />
       ))}
     </div>
+    </>
   );
 };
 
