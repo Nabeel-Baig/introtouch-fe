529ac52e7b2a208e10083ce841c89d030c62464d
Samair <samairzia1995@gmail.com>
2023-06-13 00:17:15 +0500

    bio icons fix

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -67,19 +67,19 @@ const Modal = ({
                             <span className="sr-only">Close modal</span>
                         </button>
                         <div className="py-6 px-6 lg:px-8">
-                            <h3 className={`mb-4 text-xl font-medium text-gray-900 dark:text-white ${contentClass}`}>
-                                {title}
+                            <h3 className={`mb-4 text-xl font-medium text-gray-900 dark:text-white ${contentClass} ${title === 'Add Bio' ? 'flex items-center' : 'no'}`}>
+                                <span className="mr-1">{title}</span>
+                                {(title === 'Add Bio') && (
+                                    <Tooltip
+                                        content="It must provide a concise overview of your professional background, expertise, and key achievements, enabling visitors to quickly understand your qualifications and value proposition.">
+                                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24"
+                                            viewBox="0 0 24 24">
+                                            <path
+                                                d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 11 7 L 11 9 L 13 9 L 13 7 L 11 7 z M 11 11 L 11 17 L 13 17 L 13 11 L 11 11 z"></path>
+                                        </svg>
+                                    </Tooltip>
+                                )}
                             </h3>
-                            {(title === 'Add Bio') && (
-                                <Tooltip
-                                    content="It must provide a concise overview of your professional background, expertise, and key achievements, enabling visitors to quickly understand your qualifications and value proposition.">
-                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24"
-                                         viewBox="0 0 24 24">
-                                        <path
-                                            d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 11 7 L 11 9 L 13 9 L 13 7 L 11 7 z M 11 11 L 11 17 L 13 17 L 13 11 L 11 11 z"></path>
-                                    </svg>
-                                </Tooltip>
-                            )}
 
                             <div>{body}</div>
                         </div>
